b21af5f59b85eae1504eb571194f32db36f37edf
Erik Perttu <erik.perttu@gmail.com>
2017-12-26 17:10:11 +0700

    main user cli work

---
+++ cli.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"context"
+	pb "github.com/erikperttu/shippy-user-service/proto/user"
+	"github.com/micro/go-micro"
+	microClient "github.com/micro/go-micro/client"
+)
+
+func main() {
+	srv := micro.NewService(
+		micro.Name("go.micro.srv.user-cli"),
+		micro.Version("latest"),
+	)
+
+	srv.Init()
+
+	client := pb.NewUserServiceClient("go.micro.srv.user", microClient.DefaultClient)
+
+	name := "Test Name"
+	email := "test@example.com"
+	password := "test123"
+	company := "Very large ships inc."
+
+	r, err := client.Create(context.TODO(), &pb.User{
+		Name:     name,
+		Email:    email,
+		Password: password,
+		Company:  company,
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to create user: %v", err)
+	}
+	log.Printf("Created: %s", r.User.Id)
+
+	getAll, err := client.GetAll(context.Background(), &pb.Request{})
+	if err != nil {
+		log.Fatalf("Failed to list users: %v", err)
+	}
+	for _, v := range getAll.Users {
+		log.Println(v)
+	}
+
+	authResponse, err := client.Auth(context.TODO(), &pb.User{
+		Email:    email,
+		Password: password,
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to authenticate user: %s error: %v\n", email, err)
+	}
+
+	log.Printf("Your access token is: %s \n", authResponse.Token)
+
+	// We good
+	os.Exit(0)
+}
